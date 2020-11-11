a55dcb99dbd42c0e5ead8b9fbcae8d9ac1629fc3
Takadza <nigeltakadza@gmail.com>
2020-11-11 09:59:35 +0200

    FreshVotes006 adding csrf protection

--- src/main/java/com/greencoder/FreshVotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/greencoder/FreshVotes/security/WebSecurityConfiguration.java
@@ -31,7 +31,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
