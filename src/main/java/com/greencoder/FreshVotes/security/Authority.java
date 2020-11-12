4dfd954099cc4a8242d8a3066c325d831bac0e88
Takadza <nigeltakadza@gmail.com>
2020-11-12 11:45:39 +0200

    FreshVotes009 Logging int freshVotes

---
+++ src/main/java/com/greencoder/FreshVotes/security/Authority.java
@@ -0,0 +1,43 @@
+package com.greencoder.FreshVotes.security;
+
+import com.greencoder.FreshVotes.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Authority implements GrantedAuthority {
+
+    private Long id;
+    private String authority;
+    private User user;
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/test/java/com/greencoder/FreshVotes/service/UserDetailServiceTest.java
@@ -0,0 +1,27 @@
+package com.greencoder.FreshVotes.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class UserDetailServiceTest {
+
+    @Test
+    public void generateEncryptedPassword(){
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "pass123";
+        String encodedPassword = encoder.encode(rawPassword);
+
+        System.out.println(encodedPassword);
+
+        assertThat(rawPassword, not(encodedPassword));
+
+
+    }
+
+}
