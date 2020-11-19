84e6cb4fad16155feda13b1b7d2968af8a9e5435
Takadza <nigeltakadza@gmail.com>
2020-11-19 12:00:02 +0200

    FreshVotes013 adding a product entity

--- src/main/java/com/greencoder/FreshVotes/domain/User.java
+++ src/main/java/com/greencoder/FreshVotes/domain/User.java
@@ -15,6 +15,7 @@ public class User {
     private String password;
     private String name;
     private Set<Authority> authorities = new HashSet<>();
+    private Set<Product> products = new HashSet<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,6 +58,15 @@ public class User {
 
     public void setAuthorities(Set<Authority> authorities) {
         this.authorities = authorities;
+    }
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user")
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
     }
 
     @Override
